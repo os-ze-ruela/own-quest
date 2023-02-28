3fb6d579ab318dc2058f74049c35fc94c7b3a2d0
CarlosHZB <carlosziliolibraga@hotmail.com>
2023-02-28 11:36:25 -0300

    second commit

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Loginstyle } from '../../styles/Login'
+
+function Login() {
+  return (
+    <Loginstyle>SAUBDAIUSDSA</Loginstyle>
+  )
+}
+
+export default Login
