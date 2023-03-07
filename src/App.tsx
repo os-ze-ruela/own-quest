b124d642eb545e48be46a33fe0a1a9b58e7c1cc9
CarlosHZB <carlosziliolibraga@hotmail.com>
2023-03-07 14:14:39 -0300

    Routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import NewPword from './pages/NewPword';
 import Recover from './pages/Recover';
 
 function App() {
   return (
-    <div style={{}}>
-      {/* <Login/> */}
-      {/* <Recover/> */}
-      <NewPword/>
-    </div>
+    <Router>
+      <Routes>
+            <Route path="/" element={<Home />} > </Route>
+            <Route path="/login" element={<Login />} > </Route>
+            <Route path="/recover" element={<Recover />} > </Route>
+            <Route path="/newpword" element={<NewPword />} > </Route>
+        </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+    return(
+        <div>
+            <h4>Página inicial</h4>
+        </div>
+    )
+}
