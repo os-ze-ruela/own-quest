9beb5c9fe2986c531c0fd024d4099cb8d0ddde64
Gabriel Palmyro <gabripalmyro13579@gmail.com>
2023-05-19 20:56:57 -0300

    fix: explorer fixed

--- src/pages/HomeLogged/HomeLogged.tsx
+++ src/pages/HomeLogged/HomeLogged.tsx
@@ -42,13 +42,6 @@ const HomeLogged = () => {
     fetchGames()
   }, [])
 
-  const randomInt = (): number => {
-    const min = 1;
-    const max = 100;
-    const rand = min + Math.random() * (max - min);
-    return rand;
-  }
-
   const { createGame } = useContext(GameContext)
   const navigate = useNavigate()
 
